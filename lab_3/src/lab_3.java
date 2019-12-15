ecfb0d85ed407a4f46968e28c279753df7b2efd3
ov-vlad10 <56393569+ov-vlad10@users.noreply.github.com>
2019-12-15 19:35:43 +0300

    Add files via upload

---
+++ lab_3/src/lab_3.java
@@ -0,0 +1,11 @@
+public class lab_3 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10){
+            if (i == 0)
+                System.out.println(1);
+            else System.out.println((double) 1 / i);
+            i++;
+        }
+    }
+}
